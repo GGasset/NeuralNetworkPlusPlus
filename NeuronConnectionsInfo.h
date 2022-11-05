d4d6039a786f37a42064e2afce49642f316fc01d
Germán <GassetGerman@gmail.com>
2022-11-05 12:14:50 +0100

    Added NeuronConnectionInfo constructor Declared but not started NeuronConnectionsInfo::GetGradients()

--- NeuronConnectionsInfo.h
+++ NeuronConnectionsInfo.h
@@ -1,6 +1,8 @@
 #pragma once
 using namespace std;
 #include <list>
+#include "ValueGeneration.h"
+#include <tuple>
 
 class NeuronConnectionsInfo
 {
@@ -10,6 +12,18 @@ public:
 
 	list<double> Weights;
 
+	NeuronConnectionsInfo(int layerI, int previousLayerLength, double minWeight, double weightClosestTo0, double maxWeight)
+	{
+		Weights = ValueGeneration::GenerateWeigths(previousLayerLength, minWeight, weightClosestTo0, maxWeight);
 
+		tuple<list<int>, list<int>> connectedPositions = ValueGeneration::GenerateConnectedPositions(layerI, 0, previousLayerLength);
+		Xs = get<0>(connectedPositions);
+		Ys = get<1>(connectedPositions);
+	}
+
+	tuple<list<double>, list<double>> GetGradients(double biasGrad, double** previousActivations)
+	{
+
+	}
 };
 
