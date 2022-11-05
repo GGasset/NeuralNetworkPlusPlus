aad8ae3c56d645a6418d368fbb002559a2f00c31
Germán <GassetGerman@gmail.com>
2022-11-05 21:23:12 +0100

    Neuron.h: Added comments, finished GetGradients()

--- Neuron.h
+++ Neuron.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "NeuronConnectionsInfo.h";
 #include "ActivationFunctions.h"
+#include "Derivatives.h"
+using namespace std;
+#include <stdlib.h>
 #include <list>
 #include <tuple>
 
@@ -18,14 +21,34 @@ public:
 
 	}
 
-	double Execute(double** neuronActivations, ActivationFunctions::ActivationFunction activationType)
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="networkActivations"></param>
+	/// <param name="activationType"></param>
+	/// <returns>tuple<linearFunction, neuronActivation></returns>
+	tuple<double, double> Execute(double** neuronActivations, ActivationFunctions::ActivationFunction activationType)
 	{
-		return ActivationFunctions::Activate(connections.Execute(neuronActivations), activationType);
+		double activation = ActivationFunctions::Activate(connections.Execute(neuronActivations), activationType);
 	}
 
-	tuple<list<double>, list<double>> GetGradients(double activationCost, ActivationFunctions::ActivationFunction activationType)
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="linearFunction"></param>
+	/// <param name="NeuronCost"></param>
+	/// <param name="activationType"></param>
+	/// <returns>tuple<weightGradients, previousActivationGradients, biasGradient></returns>
+	tuple<list<double>, list<double>, double> GetGradients(double** networkActivations, double linearFunction, double NeuronCost, ActivationFunctions::ActivationFunction activationType)
 	{
+		double biasGradient = NeuronCost * Derivatives::DerivativeOf(linearFunction, activationType);
+		tuple<list<double>, list<double>> connectionsGradients = connections.GetGradients(biasGradient, networkActivations);
 
+		list<double> weightGradients = get<0>(connectionsGradients);
+		list<double> previousActivationsGradients = get<1>(connectionsGradients);
+
+		tuple<list<double>, list<double>, double> output(weightGradients, previousActivationsGradients, biasGradient);
+		return output;
 	}
 };
 
