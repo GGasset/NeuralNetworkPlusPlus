1fb37004a9d5ca0fea9cecf2fb1be9831b150e60
Germán <GassetGerman@gmail.com>
2022-11-05 20:46:35 +0100

    Added Derivatives:: SigmoidDerivative(), RELUDerivation(), DerivativeOf()

--- Derivatives.h
+++ Derivatives.h
@@ -1,5 +1,32 @@
+#include <math.h>
+#include <cmath>
+#include <stdlib.h>
+#include "ActivationFunctions.h"
+
 #pragma once
-class Derivatives
+static class Derivatives
 {
+	static double DerivativeOf(double linearFunction, ActivationFunctions::ActivationFunction ActivationType)
+	{
+		switch (ActivationType)
+		{
+		case ActivationFunctions::RELU:
+			return RELUDerivative(linearFunction);
+		case ActivationFunctions::Sigmoid:
+			return SigmoidDerivative(linearFunction);
+		default:
+			return 0;
+		}
+	}
+
+	static double RELUDerivative(double linearFunction)
+	{
+		return linearFunction * (linearFunction >= 0);
+	}
+
+	static double SigmoidDerivative(double linearFunction)
+	{
+		return ((-linearFunction - 1) * exp(-linearFunction - 1)) / pow(1 + exp(linearFunction), 2);
+	}
 };
 
