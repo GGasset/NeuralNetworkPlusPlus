de0f5e8910a488c99246a00a7755bd6d4f923cac
Germán <GassetGerman@gmail.com>
2022-11-06 19:47:49 +0100

    fixed warning at NeuronalNetwork::GetNetworkShape() Added NeuralNetwork.h constructor

--- NeuronalNetwork.h
+++ NeuronalNetwork.h
@@ -10,24 +10,36 @@ private:
 	ActivationFunctions::ActivationFunction ActivationFunction;
 
 public:
+	NeuronalNetwork(long shapeLength, long* shape, double bias, ActivationFunctions::ActivationFunction activationFunction, double minWeight, double weightClosestTo0, double maxWeight)
+	{
+		neurons = list<list<Neuron>>();
+		for (long i = 1; i < shapeLength; i++)
+		{
+			list<Neuron> currentLayer = list<Neuron>();
+			for (long i = 0; i < shape[i]; i++)
+			{
+				currentLayer.push_back(Neuron(i, shape[i - 1], bias, minWeight, weightClosestTo0, maxWeight));
+			}
+			neurons.push_back(currentLayer);
+		}
+
+		ActivationFunction = activationFunction;
+	}
+
 	ActivationFunctions::ActivationFunction GetActivationFunction()
 	{
 		return ActivationFunction;
 	}
 
-	long* GetNetworkShape()
+	list<long> GetNetworkShape()
 	{
 		long networkLength = GetNetworkLength();
-		long* shape = (long*)malloc(sizeof(long) * networkLength);
-		if (!shape)
-		{
-			return GetNetworkShape();
-		}
+		list<long> shape = list<long>();
 
 		auto layerIterator = neurons.cbegin();
 		for (long i = 0; i < networkLength; i++, layerIterator++)
 		{
-			(shape[i]) = (*layerIterator).size();
+			shape.push_back((*layerIterator).size());
 		}
 		return shape;
 	}
