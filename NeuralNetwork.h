31202dc228e68161b15426362139b6ae273bb732
Germán <GassetGerman@gmail.com>
2022-11-07 14:07:47 +0100

    NeuronalNetwork.h: Added ExecuteStore() / ExecuteStoreLayer() Added GetNetworkInputLength() Added XAML comment and input layer to output of GetNetworkShape()

--- NeuralNetwork.h
+++ NeuralNetwork.h
@@ -27,17 +27,63 @@ public:
 		ActivationFunction = activationFunction;
 	}
 
-	//tuple<double* ExecuteStore(double* input)
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="input"></param>
+	/// <returns>tuple(networkLinears, networkActivations)</returns>
+	tuple<double**, double**> ExecuteStore(double* input)
+	{
+		double** networkLinearFunctions = new double* [GetNetworkLayerCount()];
+		double** networkActivations = new double* [GetNetworkLayerCount() + 1];
+		networkActivations[0] = input;
+
+		auto layerIter = neurons.begin();
+		for (long i = 0; i < GetNetworkLayerCount(); i++, layerIter++)
+		{
+			tuple<double*, double*> layerExecutionResults = ExecuteStoreLayer(layerIter, networkActivations);
+			networkLinearFunctions[i] = get<0>(layerExecutionResults);
+			networkActivations[i + 1] = get<1>(layerExecutionResults);
+		}
+	}
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="layerIter"></param>
+	/// <param name="neuronActivations"></param>
+	/// <returns>tuple(linearFunctions, neuronActivations)</returns>
+	tuple<double*, double*> ExecuteStoreLayer(list<list<Neuron>>::iterator layerIter, double** neuronActivations)
+	{
+		list<Neuron> layer = (*layerIter);
+		long layerLength = layer.size();
+		double* layerLinears = new double[layerLength];
+		double* layerActivations = new double[layerLength];
+
+		auto neuronIter = layer.begin();
+		for (long i = 0; neuronIter != layer.end(); i++, neuronIter++)
+		{
+			tuple<double, double> neuronExecutionResults = (*neuronIter).ExecuteStore(neuronActivations, ActivationFunction);
+			layerLinears[i] = get<0>(neuronExecutionResults);
+			layerActivations[i] = get<1>(neuronExecutionResults);
+		}
+		tuple<double*, double*> layerExecutionResults(layerLinears, layerActivations);
+	}
 
 	ActivationFunctions::ActivationFunction GetActivationFunction()
 	{
 		return ActivationFunction;
 	}
 
+	/// <summary>
+	/// Output includes input layer
+	/// </summary>
+	/// <returns></returns>
 	list<long> GetNetworkShape()
 	{
-		long networkLength = GetNetworkLength();
+		long networkLength = GetNetworkLayerCount();
 		list<long> shape = list<long>();
+		shape.push_back(GetNetworkInputLength());
 
 		auto layerIterator = neurons.begin();
 		for (long i = 0; layerIterator != neurons.end(); i++, layerIterator++)
@@ -47,9 +93,20 @@ public:
 		return shape;
 	}
 
-	long GetNetworkLength()
+	/// <summary>
+	/// Output doesn't include input layer
+	/// </summary>
+	/// <returns></returns>
+	long GetNetworkLayerCount()
 	{
 		return neurons.size();
+	}
+
+	long GetNetworkInputLength()
+	{
+		auto layerIterator = neurons.begin();
+		auto neuronIterator = (*layerIterator).begin();
+		return (*neuronIterator).connections.GetConnectionCount();
 	}
 };
 
