db7911d5107565a396501b11d3fd95e21c914967
Germán <GassetGerman@gmail.com>
2022-11-08 15:58:14 +0100

    finished, for the moment, Cost.h

--- Cost.h
+++ Cost.h
@@ -1,9 +1,26 @@
+#include <cmath>
+
 #pragma once
 class Cost
 {
+public:
+	enum CostFunction
+	{
+		SquaredMean,
+	};
+
+	static double SquaredMeanLoss(long outputLength, double* neuronOutput, double* Y)
+	{
+		double mean = 0;
+		for (long i = 0; i < outputLength; i++)
+		{
+			mean += SquaredMeanLoss(neuronOutput[i], Y[i]);
+		}
+	}
+
 	static double SquaredMeanLoss(double neuronOutput, double Y)
 	{
-
+		return pow(Y - neuronOutput, 2);
 	}
 };
 
