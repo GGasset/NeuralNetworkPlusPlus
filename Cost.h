ae15fa2fae5f065d282b1ea1ae5050ddb2656be4
Germán <GassetGerman@gmail.com>
2022-11-08 16:03:16 +0100

    fixed bug at Cost::SquaredMeanLoss(double, double)

--- Cost.h
+++ Cost.h
@@ -9,6 +9,17 @@ public:
 		SquaredMean,
 	};
 
+	static double GetCostOf(long networkOutputLength, double* neuronOutput, double* Y, CostFunction costType)
+	{
+		switch (costType)
+		{
+		case Cost::SquaredMean:
+			return SquaredMeanLoss(networkOutputLength, neuronOutput, Y);
+		default:
+			return 0;
+		}
+	}
+
 	static double SquaredMeanLoss(long outputLength, double* neuronOutput, double* Y)
 	{
 		double mean = 0;
@@ -20,7 +31,7 @@ public:
 
 	static double SquaredMeanLoss(double neuronOutput, double Y)
 	{
-		return pow(Y - neuronOutput, 2);
+		return pow(neuronOutput - Y, 2);
 	}
 };
 
