95cafff277083789a44384ecdc71ac34be66970c
Germán <GassetGerman@gmail.com>
2022-11-08 20:58:34 +0100

    Added 2 overloads for Derivtives::SquareMeanDerivative()

--- Derivatives.h
+++ Derivatives.h
@@ -20,6 +20,7 @@ public:
 		}
 	}
 
+	// Activation function derivatives
 	static double RELUDerivative(double linearFunction)
 	{
 		return linearFunction * (linearFunction >= 0);
@@ -28,6 +29,22 @@ public:
 	static double SigmoidDerivative(double linearFunction)
 	{
 		return ((-linearFunction - 1) * exp(-linearFunction - 1)) / pow(1 + exp(linearFunction), 2);
+	}
+
+	static double* SquaredMeanDerivative(long networkOutputLength, double* networkOutput, double* Y)
+	{
+		double* output = new double[networkOutputLength];
+		for (long i = 0; i < networkOutputLength; i++)
+		{
+			output[i] = SquaredMeanDerivative(networkOutput[i], Y[i]);
+		}
+		return output;
+	}
+
+	//Cost derivatives
+	static double SquaredMeanDerivative(double neuronOutput, double Y)
+	{
+		return 2 * (neuronOutput - Y);
 	}
 };
 
