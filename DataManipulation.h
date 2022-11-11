f0602c2d92becd2e05da49854e74d0796167d087
Germán <GassetGerman@gmail.com>
2022-11-11 21:08:09 +0100

    added 2 overloads for DataManipulation::AddLists(), one for size_t value lists and the other float value lists

--- DataManipulation.h
+++ DataManipulation.h
@@ -1,5 +1,29 @@
+#include <list>
+
 #pragma once
 class DataManipulation
 {
+public:
+	static void AddLists(std::list<size_t>* first, std::list<size_t> second)
+	{
+		std::list<size_t>::iterator secondListIter = second.begin();
+		while (secondListIter != second.end())
+		{
+			first[0].push_back((*secondListIter));
+
+			secondListIter++;
+		}
+	}
+
+	static void AddLists(std::list<float>* first, std::list<float> second)
+	{
+		std::list<float>::iterator secondListIter = second.begin();
+		while (secondListIter != second.end())
+		{
+			first[0].push_back((*secondListIter));
+
+			secondListIter++;
+		}
+	}
 };
 
