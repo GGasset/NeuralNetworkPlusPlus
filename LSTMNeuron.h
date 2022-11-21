52673f8170212a6c00884f06c1d5aca19194d3d1
Germán <GassetGerman@gmail.com>
2022-11-21 21:25:59 +0100

    dercreased memory usage at LSTMNeuron by removing a parameter that can be declared on RecurrentNeuralNetwork in the future

--- LSTMNeuron.h
+++ LSTMNeuron.h
@@ -5,16 +5,12 @@
 #pragma once
 class LSTMNeuron
 {
-private:
-	ActivationFunctions::ActivationFunction ActivationFunction;
-
 public:
 	NeuronConnectionsInfo connections;
 	float ForgetGateWeight, StoreGateWeight, OutputGateWeight;
 
-	LSTMNeuron(size_t layerI, size_t previousLayerLength, ActivationFunctions::ActivationFunction activationFunction, float bias, float minWeight, float weightClosestTo0, float maxWeight)
+	LSTMNeuron(size_t layerI, size_t previousLayerLength, float bias, float minWeight, float weightClosestTo0, float maxWeight)
 	{
-		ActivationFunction = activationFunction;
 		connections = NeuronConnectionsInfo(layerI, previousLayerLength, bias, minWeight, weightClosestTo0, maxWeight);
 		ForgetGateWeight = ValueGeneration::GenerateWeight(minWeight, weightClosestTo0, maxWeight);
 		StoreGateWeight = ValueGeneration::GenerateWeight(minWeight, weightClosestTo0, maxWeight);
