823ed812c755a3c7c3a67605f1bc3545bccaec57
Germán <GassetGerman@gmail.com>
2022-11-22 19:54:41 +0100

    Added hidden/cell states at LSTMNeuron Started LSTMNeuron.ExecuteStore()

--- LSTMNeuron.h
+++ LSTMNeuron.h
@@ -1,12 +1,14 @@
 #include "ValueGeneration.h"
 #include "NeuronConnectionsInfo.h"
 #include "ActivationFunctions.h"
+#include "NeuronStoredValues.h"
 
 #pragma once
 class LSTMNeuron
 {
 public:
 	NeuronConnectionsInfo connections;
+	float hiddenState, cellState;
 	float ForgetGateWeight, StoreGateSigmoidWeight, StoreGateTanhWeight, OutputGateWeight;
 
 	LSTMNeuron(size_t layerI, size_t previousLayerLength, float bias, float minWeight, float weightClosestTo0, float maxWeight)
@@ -16,6 +18,15 @@ public:
 		StoreGateSigmoidWeight = ValueGeneration::GenerateWeight(minWeight, weightClosestTo0, maxWeight);
 		StoreGateTanhWeight = ValueGeneration::GenerateWeight(minWeight, weightClosestTo0, maxWeight);
 		OutputGateWeight = ValueGeneration::GenerateWeight(minWeight, weightClosestTo0, maxWeight);
+		hiddenState = 0;
+		cellState = 0;
+	}
+
+	NeuronStoredValues ExecuteStore(float** networkActivations)
+	{
+		NeuronStoredValues output;
+		output.LinearFunction = connections.LinearFunction(networkActivations);
+		
 	}
 };
 
