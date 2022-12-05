ebc86158d8b537323ed94022cef32283eba8e20b
Germán <GassetGerman@gmail.com>
2022-12-05 15:11:49 +0100

    declared fields at NeuronHolder.h

--- NeuronHolder.h
+++ NeuronHolder.h
@@ -1,5 +1,17 @@
+#include "Neuron.h"
+#include "LSTMNeuron.h"
+
 #pragma once
 class NeuronHolder
 {
+public:
+	enum NeuronType
+	{
+		LSTM,
+		NonRecurrentNeuron
+	};
+
+	Neuron* Neuron;
+	LSTMNeuron* LstmNeuron;
 };
 
