4a70ab01a3a7ce9edccdab5e1e93097d273f2eb4
Germán <GassetGerman@gmail.com>
2022-12-05 15:36:48 +0100

    Declared some INeuron functions

--- INeuron.h
+++ INeuron.h
@@ -1,5 +1,14 @@
+#include "NeuronConnectionsInfo.h"
+#include "NeuronStoredValues.h"
+
 #pragma once
 class INeuron
 {
+public:
+	NeuronConnectionsInfo connections;
+
+	virtual float Execute(float** neuronActivations, ActivationFunctions::ActivationFunction activationType) = 0;
+
+	virtual NeuronStoredValues RecurrentExecuteStore(float** networkActivations, ActivationFunctions::ActivationFunction activationType) = 0;
 };
 
