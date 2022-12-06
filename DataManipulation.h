80ff7d5cc638e140eb3524c077cb98aef507f071
Germán <GassetGerman@gmail.com>
2022-12-06 20:31:03 +0100

    Solved unknown bug by simplifying function and not returning a tuple :(

--- DataManipulation.h
+++ DataManipulation.h
@@ -1,7 +1,8 @@
 #include <cmath>
+#include <tuple>
 
 #pragma once
-class DataManipulation
+static class DataManipulation
 {
 public:
 	static void ShuffleData(float** data, size_t dataLength)
@@ -18,30 +19,11 @@ public:
 		}
 	}
 
-	/// <returns>tuple<first slice, second slice, first slice length></returns>
-	static tuple<float**, float**, size_t> SliceData(float** data, size_t dataLength, double slicePoint, bool deleteOldArray)
+	static size_t GetSlicePoint(size_t dataLength, double slicePoint)
 	{
 		slicePoint = fabs(slicePoint);
-		slicePoint *= 1 + (dataLength - 1) * slicePoint > 1;
+		slicePoint *= 1 + (dataLength - 1) * (slicePoint > 1);
 		slicePoint = (double)(size_t)(slicePoint);
-
-		float** firstSlice = new float* [slicePoint];
-		float** secondSlice = new float* [dataLength - (size_t)slicePoint];
-
-		for (size_t i = 0; i < slicePoint; i++)
-		{
-			firstSlice[i] = data[i];
-		}
-
-		for (size_t i = 0; i < dataLength - slicePoint; i++)
-		{
-			secondSlice[i] = data[(size_t)slicePoint + i];
-		}
-
-		if (deleteOldArray)
-			delete[] data;
-
-		tuple<float**, float**, size_t> output(firstSlice, secondSlice, (size_t)slicePoint);
-		return output;
+		return (size_t)slicePoint;
 	}
 };
--- NeuralNetwork.h
+++ NeuralNetwork.h
@@ -201,40 +201,31 @@ public:
 	/// <param name="trainUntilTestCostIsBelow">Put a high value to iterate once over all data</param>
 	void SupervisedTrain(float** X, float** Y, size_t dataLength, size_t batchSize, Cost::CostFunction costFunction, float learningRate, bool freeData, double testSize = 0.2, double trainUntilTestCostIsBelow = .15)
 	{
-		size_t trainSize = std::fmaxf(0, std::fminf(1, testSize));
+		float trainSize = (float)fmaxf(testSize / (1 + (dataLength) * testSize > 1), 0);
 		trainSize = 1 - testSize;
 
 		DataManipulation::ShuffleData(X, dataLength);
 		DataManipulation::ShuffleData(Y, dataLength);
 
-		tuple<float**, float**, size_t> slicedX = DataManipulation::SliceData(X, dataLength, trainSize, freeData);
-		float** trainX = get<0>(slicedX);
-		float** testX = get<1>(slicedX);
-
-		tuple<float**, float**, size_t> slicedY = DataManipulation::SliceData(Y, dataLength, trainSize, freeData);
-		float** trainY = get<0>(slicedY);
-		float** testY = get<1>(slicedY);
-
-		size_t trainDataLength = min(get<2>(slicedX), get<2>(slicedY));
-		size_t testDataLength = dataLength - max(get<2>(slicedX), get<2>(slicedY));
+		size_t slicePoint = DataManipulation::GetSlicePoint(dataLength, trainSize);
 
 		float testCost = 50E30f;
-		size_t batchCount = trainDataLength / batchSize;
-		size_t lastBatchSize = trainDataLength % batchSize;
+		size_t batchCount = slicePoint / batchSize;
+		size_t lastBatchSize = slicePoint % batchSize;
 		while (testCost > trainUntilTestCostIsBelow)
 		{
 			for (size_t i = 0; i < batchCount; i++)
 			{
-				SupervisedLearningBatch(trainX, trainY, batchSize * i, batchSize, costFunction, learningRate);
+				SupervisedLearningBatch(X, Y, batchSize * i, batchSize, costFunction, learningRate);
 			}
-			SupervisedLearningBatch(trainX, trainY, batchCount * batchSize, lastBatchSize, costFunction, learningRate);
+			SupervisedLearningBatch(X, Y, batchCount * batchSize, lastBatchSize, costFunction, learningRate);
 
 			testCost = 0;
-			for (size_t i = 0; i < testDataLength; i++)
+			for (size_t i = slicePoint; i < dataLength; i++)
 			{
-				testCost += Cost::GetCostOf(OutputLength, Execute(testX[i]), testY[i], costFunction);
+				testCost += Cost::GetCostOf(OutputLength, Execute(X[i]), Y[i], costFunction);
 			}
-			testCost /= testDataLength;
+			testCost /= dataLength - slicePoint;
 		}
 	}
 
