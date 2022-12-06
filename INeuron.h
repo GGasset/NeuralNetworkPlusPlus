71f98d708e5bbbc8d19ff58ac93262bfd3e2e7ae
Germán <GassetGerman@gmail.com>
2022-12-06 13:41:31 +0100

    Finished INeuron.h

--- INeuron.h
+++ INeuron.h
@@ -10,5 +10,9 @@ public:
 	virtual float Execute(float** neuronActivations, ActivationFunctions::ActivationFunction activationType) = 0;
 
 	virtual NeuronStoredValues RecurrentExecuteStore(float** networkActivations, ActivationFunctions::ActivationFunction activationType) = 0;
+
+	virtual tuple<NeuronConnectionsInfo, float*> GetRecurrentGradients(size_t tCount, NeuronStoredValues storedExecution, float neuronCost, float*** networkCosts, float*** networkActivations) = 0;
+
+	virtual void ApplyGradients(NeuronConnectionsInfo connections, float* fieldsGradient) = 0;
 };
 
