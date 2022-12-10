40e99710a465ac5cb640cc9ddd4acefe52fb79d0
Germán <GassetGerman@gmail.com>
2022-12-10 14:01:13 +0100

    Setted the base for RNN.h class

--- RNN.h
+++ RNN.h
@@ -1,6 +1,27 @@
+#include "INeuron.h"
+#include "Neuron.h"
+#include "LSTMNeuron.h"
+
 #pragma once
 class RNN
 {
+public:
+	enum NeuronType
+	{
+		Neuron,
+		LSTMNeuron
+	};
 
+private:
+	INeuron*** neurons;
+	ActivationFunctions::ActivationFunction activationFunction;
+	size_t shapeLength;
+	int* shape;
+
+public:
+	RNN(size_t layerCount, int* shape, NeuronType* layerTypes, ActivationFunctions::ActivationFunction activationFunction)
+	{
+
+	}
 };
 
